def8e9aa86e7f533927e6e3c242a391ae17ab015
Rishabh Garg <rishabhgarg108@gmail.com>
2021-05-25 15:44:14 +0530

    Add files to create db

---
+++ createDB.py
@@ -0,0 +1,17 @@
+import random
+
+filename = "data.txt"
+n = 1000000
+
+with open(filename, 'w') as f:
+    for idx in range(1, n + 1):
+        row = [str(idx)]
+        numItems = random.randint(1, 50)
+        
+        for k in range(numItems):
+            price = random.randint(100, 5000)
+            row.append(str(price))
+        f.write(' '.join(row) + '\n')
+
+        if (idx % 10000 == 0):
+            print(idx)
---
+++ glimpse.py
@@ -0,0 +1,5 @@
+filename = 'data.txt'
+
+with open(filename, 'r') as f:
+    for i in range(10):
+        print(f.readline())
