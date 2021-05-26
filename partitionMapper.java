c81226f1974fc9e7688b3ddd1aa59aff92afd72e
Rishabh Garg <rishabhgarg108@gmail.com>
2021-05-26 20:23:03 +0530

    Final commit

--- partitionMapper.java
+++ partitionMapper.java
@@ -27,11 +27,11 @@ public class partitionMapper extends Mapper <LongWritable, Text, Text, Text> {
 		}
 
 		
-		if (count >= 1 && count <= 10) category.set("class1");
-		else if (count >= 11 && count <= 20) category.set("class2");
-		else if (count >= 21 && count <= 30) category.set("class3");
-		else if (count >= 31 && count <= 40) category.set("class4");
-		else if (count >= 41 && count <= 50) category.set("class5");
+		if (count >= 1 && count <= 10) category.set("class1,");
+		else if (count >= 11 && count <= 20) category.set("class2,");
+		else if (count >= 21 && count <= 30) category.set("class3,");
+		else if (count >= 31 && count <= 40) category.set("class4,");
+		else if (count >= 41 && count <= 50) category.set("class5,");
 		
 		total.set(Long.toString(sum));
 
