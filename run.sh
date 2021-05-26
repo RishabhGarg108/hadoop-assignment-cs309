17afb36e6da2a1858cf48db7f0182ff014a951e1
ParasJain-ui <parasjain1237@gmail.com>
2021-05-26 19:58:00 +0530

    finalgit add .

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 javac -d . partitionDriver.java partitionMapper.java partitionReducer.java
 jar cfm pd.jar Manifest.txt assignment/*.class
+$HADOOP_HOME/bin/hdfs dfs -rm -r /input
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal ./input /
 $HADOOP_HOME/bin/hdfs dfs -rm -r /output
 $HADOOP_HOME/bin/hadoop jar pd.jar /input /output
+$HADOOP_HOME/bin/hdfs dfs -cat /output/part-r-00000
